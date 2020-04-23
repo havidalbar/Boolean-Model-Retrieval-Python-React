7ec794697b58f1ae29d01ac9f847bbf2d64488b3
havidalbar@gmail.com <havidalbar@gmail.com>
2020-04-23 20:49:24 +0700

    refactor file infix_to_postfix

---
+++ server/retrieval/infix_to_postfix.py
@@ -0,0 +1,29 @@
+from utils import stack
+import re
+
+def infix_to_postfix(query):
+    operator_stack = stack()
+    postfix = stack()
+    precedence = {'(':0,')':0,'or':1,'and':2,'not':3}
+
+    list_token = re.findall(r"([a-zA-Z]+|\(|\))", query)
+    for token in list_token:
+        if token not in precedence:
+            postfix.push(token)
+        elif token == '(':
+            operator_stack.push(token)
+        elif token == ')':
+            head = operator_stack.pop()
+            while head != '(':
+                postfix.push(head)
+                head = operator_stack.pop()
+        else:
+            while operator_stack.__len__() > 0 and (precedence[operator_stack.peek()] >= precedence[token]):
+                postfix.push(operator_stack.pop())
+            operator_stack.push(token)
+
+    while operator_stack.__len__() > 0:
+        postfix.push(operator_stack.pop())
+    return postfix.get_stack()
+
+
