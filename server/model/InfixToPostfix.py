7766522ce7612fe04656d5062806692c91593c08
havidalbar@gmail.com <havidalbar@gmail.com>
2020-04-23 10:06:11 +0700

    implement class stack in infix to postfix

--- server/model/InfixToPostfix.py
+++ server/model/InfixToPostfix.py
@@ -1,31 +1,53 @@
 import re
 
+class stack:
+    def __init__(self):
+        self.stack = []
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def is_empty(self):
+        return self.stack == []
+
+    def peek(self):
+        if not self.is_empty():
+            return self.stack[-1]
+
+    def get_stack(self):
+        return self.stack
+
+    def __len__(self):
+        return len(self.stack)
+
+
 def infix_to_postfix(query):
-	list_operator = ['(', ')', 'or', 'and', 'not']
-	priority_operator = [0, 0, 1, 2, 3]
-	operator_stack = []
-	postfix = []
-	precedence = {}
-	for i in range(len(list_operator)):
-		precedence[list_operator[i]] = priority_operator[i]
+    list_operator = ['(', ')', 'or', 'and', 'not']
+    priority_operator = [0, 0, 1, 2, 3]
+    operator_stack = stack()
+    postfix = stack()
+    precedence = {}
+    for i in range(len(list_operator)):
+        precedence[list_operator[i]] = priority_operator[i]
 
-	list_token = re.findall(r"([a-zA-Z]+|\(|\))", query)
-	for token in list_token:
-		if token not in precedence:
-			postfix.append(token)
-		elif token == '(':
-			operator_stack.append(token)
-		elif token == ')':
-			head = operator_stack.pop()
-			while head != '(':
-				postfix.append(head)
-				head = operator_stack.pop()
-		else:
-			while len(operator_stack) > 0 and (precedence[operator_stack[-1]] >= precedence[token]):
-				postfix.append(operator_stack.pop())
-			operator_stack.append(token)
+    list_token = re.findall(r"([a-zA-Z]+|\(|\))", query)
+    for token in list_token:
+        if token not in precedence:
+            postfix.push(token)
+        elif token == '(':
+            operator_stack.push(token)
+        elif token == ')':
+            head = operator_stack.pop()
+            while head != '(':
+                postfix.push(head)
+                head = operator_stack.pop()
+        else:
+            while operator_stack.__len__() > 0 and (precedence[operator_stack.get_stack()[-1]] >= precedence[token]):
+                postfix.push(operator_stack.pop())
+            operator_stack.push(token)
 
-	while len(operator_stack) > 0:
-		postfix.append(operator_stack.pop())
-	return postfix
-
+    while operator_stack.__len__() > 0:
+        postfix.push(operator_stack.pop())
+    return postfix.get_stack()
