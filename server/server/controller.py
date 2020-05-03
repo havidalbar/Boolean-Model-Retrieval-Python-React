4b10578b575f94a17c01efec92c9ba664d6a07a7
Fadhil Yusuf Rahadika <fadhilyusuf27@gmail.com>
2020-05-03 22:30:47 +0700

    Implement pagination in Search API

--- server/server/controller.py
+++ server/server/controller.py
@@ -14,13 +14,29 @@ def search(request: web.Request):
     boolean_model: BooleanModel = load_model_pickle()
     try:
         query: str = request.query.get('q', '').strip()
+        current_page: int = int(request.query.get('page', 1))
+        page_size_limit: int = int(request.query.get('limit', 10))
+
         postfix_query: List[int] = infix_to_postfix(query)
         evaluator_result: Set[int] = postfix_evaluator(postfix_query, boolean_model)
 
         result_docs: List[Dict[str, str]] = [doc.asdict(output_keys=['slug', 'title', 'img', 'cleaned'])
                                              for doc in boolean_model.get_documents(evaluator_result)]
+        
+        total_result: int = len(result_docs)
+        offset: int = (current_page - 1) * page_size_limit
+        result_docs = result_docs[offset:current_page * page_size_limit]
 
-        return web.json_response({"message": "Success!", "data": result_docs})
+        return web.json_response({
+            "message": "Success!",
+            "meta": {
+                "page": current_page,
+                "size": len(result_docs),
+                "limit": page_size_limit,
+                "total": total_result
+            },
+            "data": result_docs
+        })
     except:
         return web.json_response({"message": "Invalid Query!"}, status=422)
 
